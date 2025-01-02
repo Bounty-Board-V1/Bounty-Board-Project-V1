2b026e6e8a075ceff7c1606b92c5f8ea56346505
Mohammed Odat <mohammed.a.odatt@gmail.com>
2025-01-02 15:13:17 +0300

    Normalize line endings

--- backend/src/controllers/milestoneController.js
+++
@@ -1,5 +0,0 @@
-exports.getAllMilestones = (req, res) => res.send("Get all milestones");
-exports.getMilestone = (req, res) => res.send("Get a single milestone");
-exports.createMilestone = (req, res) => res.send("Create a new milestone");
-exports.updateMilestone = (req, res) => res.send("Update milestone");
-exports.deleteMilestone = (req, res) => res.send("Delete milestone");
--- backend/src/controllers/rewardController.js
+++
@@ -1,5 +0,0 @@
-exports.getAllRewards = (req, res) => res.send("Get all rewards");
-exports.getReward = (req, res) => res.send("Get a single reward");
-exports.createReward = (req, res) => res.send("Create a new reward");
-exports.updateReward = (req, res) => res.send("Update reward");
-exports.deleteReward = (req, res) => res.send("Delete reward");
